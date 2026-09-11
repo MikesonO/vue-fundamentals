89b11bc54c7cf5449503f3a864fbd1116fc295e6
mikeson <mikeson.jnr@gmail.com>
2026-09-11 21:57:57 +0100

    Step 7: List Rendering

---
+++ 07-list-rendering/App.vue
@@ -0,0 +1,43 @@
+<script>
+// give each todo a unique id
+let id = 0
+
+export default {
+
+  // reactive state for the new todo and the list of todos
+  data() {
+    return {
+      newTodo: '',
+      todos: [
+        { id: id++, text: 'Learn HTML' },
+        { id: id++, text: 'Learn JavaScript' },
+        { id: id++, text: 'Learn Vue' }
+      ]
+    }
+  },
+
+  // methods for adding and removing todos
+  methods: {
+    addTodo() {
+      this.todos.push({ id: id++, text: this.newTodo })
+      this.newTodo = ''
+    },
+    removeTodo(todo) {
+      this.todos = this.todos.filter((t) => t !== todo)
+    }
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" required placeholder="new todo">
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+</template>
