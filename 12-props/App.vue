7c9f7a20a3afa3e271db3dbd95dde791cc20f1c4
mikeson <mikeson.jnr@gmail.com>
2026-09-13 10:31:15 +0100

    Step 12: Props

---
+++ 12-props/App.vue
@@ -0,0 +1,20 @@
+<script>
+import ChildComp from './ChildComp.vue'
+
+export default {
+  components: {
+    ChildComp
+  },
+  data() {
+    return {
+      // Greeting message to be passed as a prop to the child component
+      greeting: 'Hello from parent'
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- Passing the greeting data as a prop to the child component -->
+  <ChildComp :msg="greeting" />
+</template>
---
+++ 12-props/ChildComp.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  // Defining the props that this component expects to receive from its parent
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<template>
+  <!-- Displaying the message passed from the parent component -->
+  <h2>{{ msg || 'No props passed yet' }}</h2>
+</template>
