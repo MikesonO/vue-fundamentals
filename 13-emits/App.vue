844b3854dda8cd1c0ebb967e8d21eaf19520443d
mikeson <mikeson.jnr@gmail.com>
2026-09-13 10:55:36 +0100

    Step 13: Emits

---
+++ 13-emits/App.vue
@@ -0,0 +1,20 @@
+<script>
+import ChildComp from './ChildComp.vue'
+
+export default {
+  components: {
+    ChildComp
+  },
+  data() {
+    return {
+      childMsg: 'No child msg yet'
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- Listening for the response event from the child component -->
+  <ChildComp @response="(msg) => childMsg = msg" />
+  <p>{{ childMsg }}</p>
+</template>
---
+++ 13-emits/ChildComp.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  // Declaring the events that this component can emit to its parent
+  emits: ['response'],
+  created() {
+    // Emitting the 'response' event with a message when the component is created
+    this.$emit('response', 'hello from child')
+  }
+}
+</script>
+
+<template>
+  <h2>Child component</h2>
+</template>
