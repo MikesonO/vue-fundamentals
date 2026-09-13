f288fa4e9aa09e1d3ba3aed684b058b940ef12af
mikeson <mikeson.jnr@gmail.com>
2026-09-13 10:14:01 +0100

    Step 11: Component

---
+++ 11-components/App.vue
@@ -0,0 +1,17 @@
+<script>
+
+// Importing the child component to be used in this parent component
+import ChildComp from './ChildComp.vue'
+  
+export default {
+  // Registering the imported child component so it can be used in the template
+  components: {
+    ChildComp
+  }
+}
+</script>
+
+<template>
+  <!-- Using the child component -->
+ <ChildComp />
+</template>
