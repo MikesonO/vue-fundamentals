a39e1622c720fcabd5904b0a624a689dff9818cd
mikeson <mikeson.jnr@gmail.com>
2026-09-13 11:02:28 +0100

    Step 14: Slots

---
+++ 14-slots/App.vue
@@ -0,0 +1,19 @@
+<script>
+import ChildComp from './ChildComp.vue'
+
+export default {
+  components: {
+    ChildComp
+  },
+  data() {
+    return {
+      msg: 'from parent'
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- Passing content to the child component via the default slot -->
+  <ChildComp>This message is {{ msg }}</ChildComp>
+</template>
---
+++ 14-slots/ChildComp.vue
@@ -0,0 +1,4 @@
+<template>
+  <!-- Slot for content passed from the parent component or fallback content if none is provided -->
+  <slot>Fallback content</slot>
+</template>
