cde32dd864cdeb4501fc18ea4ee5e2c65599d2ff
mikeson <mikeson.jnr@gmail.com>
2026-09-13 09:52:59 +0100

    Step 9: Lifecycle and Template Refs

---
+++ 09-lifecycle-and-template-refs/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  
+  // Lifecycle hook called when the component is mounted (element exists in the DOM)
+  mounted() {
+    this.$refs.pElementRef.textContent = "Mounted test"
+  }
+}
+</script>
+
+<template> 
+  <p ref="pElementRef">Hello</p>
+</template>
